16bb593a561c9cf0532eefcfaf09294bf52c3802
SilvestriStefano <stefano.silvestri89@gmail.com>
2023-02-17 17:32:38 +0100

    remove old files and create 11ty initial files

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+module.exports = function (eleventyConfig) {
+  // Passthrough
+    //fonts
+  eleventyConfig.addPassthroughCopy("./src/assets/fonts/");
+    //css
+  eleventyConfig.addPassthroughCopy("./src/assets/css/");
+    //js
+  eleventyConfig.addPassthroughCopy("./src/assets/js/");
+    //favicon
+  eleventyConfig.addPassthroughCopy("./src/assets/img/favicon/*.(svg|ico)");
+  
+  // Plugin
+    // navigation
+  eleventyConfig.addPlugin( require("@11ty/eleventy-navigation") );
+
+  // Libraries
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
+    
+  //Shortcodes
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode("navlist", require("./lib/shortcodes/navlist.js"));
+  eleventyConfig.addShortcode("accordionItem", require("./lib/shortcodes/accordionItem.js"));
+  eleventyConfig.addAsyncShortcode('image', require('./lib/shortcodes/image.js'));
+  eleventyConfig.addAsyncShortcode('imageGroup', require('./lib/shortcodes/figureGroup.js'));
+    
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      },
+    markdownTemplateEngine: "njk",
+    templateFormats: ["md"," njk"]
+    };
+};
