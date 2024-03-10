25a358274d511ae266fa6a5e3686ef41706f4dd4
Stefano Silvestri <SilvestriStefano@users.noreply.github.com>
2024-03-10 12:09:10 +0100

    add html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
   // Passthrough
@@ -38,6 +39,21 @@ module.exports = function (eleventyConfig) {
     }
   });
     
+
+  eleventyConfig.addTransform("htmlmin", function(content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
   return {
     dir: {
       input: "src",
