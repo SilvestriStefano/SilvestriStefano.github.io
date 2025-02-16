dd377e30cdb0f337277553efd0ad86dbcc6eb339
Stefano Silvestri <SilvestriStefano@users.noreply.github.com>
2025-02-16 09:44:06 +0100

    add ribbons

--- _config/shortcodes/async/showcase.js
+++ _config/shortcodes/async/showcase.js
@@ -7,7 +7,7 @@ async function visualizationsList(items) {
         let li = '<li class="flex card">';
         let picture = await imageBg(item.thumbnail, ["card-img"], item.alt);
         li += picture;
-        li += `<article class="flex card-description" style="--tag:'${item.language}'"><h4>${md.renderInline(item.title)}</h4><p>${md.renderInline(item.description)}</p>`;
+        li += `<article class="flex card-description"><h4>${md.renderInline(item.title)}</h4><p>${md.renderInline(item.description)}</p><span class="ribbon" aria-label="language">${item.language}</span>`;
         li += `<a href="${item.link}" class="card-link">open</a></article></li>`;
         return li;
     }));
@@ -19,9 +19,9 @@ async function websList(items) {
         let li = '<li class="flex card">';
         let picture = await imageBg(item.thumbnail, ["card-img"], item.alt);
         li += picture;
-        li += `<article class="flex card-description" style="--tag:'${item.framework}'"><h4>${md.renderInline(item.title)}</h4><p>${md.renderInline(item.description)}</p>`;
+        li += `<article class="flex card-description"><h4>${md.renderInline(item.title)}</h4><p>${md.renderInline(item.description)}</p>`;
         if (item.details.length > 0) li += `<p><strong>Details:</strong> ${item.details.join('; ')}.</p>`;
-        li += `<a href="${item.link}" class="card-link">homepage</a>`
+        li += `<span class="ribbon" aria-label="framework">${item.framework}</span><a href="${item.link}" class="card-link">homepage</a>`
         if (item.source) li += `<a href="${item.source}" class="card-link">source</a>`;
         li += '</article></li>';
         return li;
